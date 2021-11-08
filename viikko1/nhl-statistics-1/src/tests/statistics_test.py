5d083d09549da07c9b071542384cf3fd116a2c7d
tirhelen <79860695+tirhelen@users.noreply.github.com>
2021-11-08 21:54:14 +0200

    Add files via upload

---
+++ viikko1/nhl-statistics-1/src/tests/statistics_test.py
@@ -0,0 +1,25 @@
+import unittest
+from statistics import Statistics
+from player import Player
+
+class PlayerReaderStub:
+    def get_players(self):
+        return [
+            Player("Semenko", "EDM", 4, 12),
+            Player("Lemieux", "PIT", 45, 54),
+            Player("Kurri",   "EDM", 37, 53),
+            Player("Yzerman", "DET", 42, 56),
+            Player("Gretzky", "EDM", 35, 89)
+        ]
+
+class TestStatistics(unittest.TestCase):
+    def setUp(self):
+        # annetaan Statistics-luokan oliolle "stub"-luokan olio
+        self.statistics = Statistics(
+            PlayerReaderStub()
+        )
+    
+    def test_konstruktori(self):
+        print(self.statistics._players)
+        print(PlayerReaderStub().get_players())
+        self.assertListEqual(self.statistics._players, PlayerReaderStub().get_players())
