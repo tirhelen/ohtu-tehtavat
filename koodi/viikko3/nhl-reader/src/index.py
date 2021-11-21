82c5e5962eba3775af259a55a94416a2772288c5
Helen Tirkkonen <helen.tirkkonen@helsinki.fi>
2021-11-21 13:49:31 +0200

    teht 1

--- koodi/viikko3/nhl-reader/src/index.py
+++ koodi/viikko3/nhl-reader/src/index.py
@@ -1,7 +1,25 @@
+import requests
 from player import Player
 
 def main():
-    pass
+    url = "https://nhlstatisticsforohtu.herokuapp.com/players"
+    response = requests.get(url).json()
 
+    print("JSON-muotoinen vastaus:")
+    print(response)
+
+    players = []
+
+    for player_dict in response:
+        player = Player(
+            player_dict['name'], player_dict['nationality'], player_dict['goals'], player_dict['assists'] )
+
+        players.append(player)
+
+    print("Oliot:")
+
+    for player in players:
+        print(player)
+        
 if __name__ == "__main__":
     main()
--- koodi/viikko3/nhl-reader/src/player.py
+++ koodi/viikko3/nhl-reader/src/player.py
@@ -1,6 +1,9 @@
 class Player:
-    def __init__(self, name):
+    def __init__(self, name, nationality, goals, assists):
         self.name = name
-    
+        self.nationality = nationality
+        self.goals = goals
+        self.assists = assists
+
     def __str__(self):
-        return self.name
+        return self.name + " from " + self.nationality + " goals: " + str(self.goals) + " assists: " + str(self.assists)
