74bc7f978b7bfb7563f756fc3bee530a4688a548
Helen Tirkkonen <helen.tirkkonen@helsinki.fi>
2021-11-29 16:46:18 +0200

    yhden tuotteen lisäämisen jälkeen ostoskorissa yksi tuote

--- koodi/viikko4/tdd-ostoskori/src/ostoskori.py
+++ koodi/viikko4/tdd-ostoskori/src/ostoskori.py
@@ -17,8 +17,7 @@ class Ostoskori:
         # kertoo korissa olevien ostosten yhteenlasketun hinnan
 
     def lisaa_tuote(self, lisattava: Tuote):
-        # lisää tuotteen
-        pass
+        self.tuotteet.append(lisattava)
 
     def poista_tuote(self, poistettava: Tuote):
         # poistaa tuotteen
