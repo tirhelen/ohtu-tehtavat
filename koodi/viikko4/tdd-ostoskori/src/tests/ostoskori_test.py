8256bc3df34d2c32e05ae686e3739dc2ef5373e7
Helen Tirkkonen <helen.tirkkonen@helsinki.fi>
2021-11-29 16:53:57 +0200

    kaksi tuotetta ostoskorin määrä oikein

--- koodi/viikko4/tdd-ostoskori/src/tests/ostoskori_test.py
+++ koodi/viikko4/tdd-ostoskori/src/tests/ostoskori_test.py
@@ -26,3 +26,11 @@ class TestOstoskori(unittest.TestCase):
         self.kori.lisaa_tuote(maito)
         self.kori.lisaa_tuote(kissa)
         self.assertEqual(self.kori.hinta(), 8)
+    
+    def test_kahden_tuotteen_lisaamisen_jalkeen_korissa_kaksi_tavaraa(self):
+        maito = Tuote("Maito", 3)
+        kissa = Tuote("Kissa", 5)
+        self.kori.lisaa_tuote(maito)
+        self.kori.lisaa_tuote(kissa)
+        self.assertEqual(self.kori.tavaroita_korissa(), 2)
+    
